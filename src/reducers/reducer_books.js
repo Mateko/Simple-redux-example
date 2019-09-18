030b2209d670b98aeb7899c1a8e77d9ada2c4f58
Mateko <mateuszpierzchala01@gmail.com>
2019-09-18 21:58:44 +0200

    simple redux project

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export function BooksReducer() {
+    return [
+        { title: 'A Book', pages: 100},
+        { title: 'B Book', pages: 120},
+        { title: 'C Book', pages: 140},
+        { title: 'D Book', pages: 160},
+        { title: 'E Book', pages: 180}
+    ]
+}
